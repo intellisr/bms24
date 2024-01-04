a344f929ba10336fda00706cac4d1407dba61d56
Sathira Rumal <sathira@chiefdx.com>
2024-01-04 22:26:05 +0530

    initial commit

---
+++ src/views/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin = () => {
+   return (
+       <div>
+           <h1>Welcome to Admin Page</h1>
+           <p>This is some dummy data for the Admin page.</p>
+       </div>
+   );
+};
+
+export default Admin;
